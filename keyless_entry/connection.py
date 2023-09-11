78f80e33da9a5bd3a7639b75dedb06ddd9bff01f
Shifi23 <shuhrat.talukder@gmail.com>
2023-09-10 22:50:21 -0700

    add loging and sysd

--- keyless_entry/connection.py
+++ keyless_entry/connection.py
@@ -1,72 +1,69 @@
 import bluetooth
+import logging
 import time
 import sys
 from AR_RPI import *
+from systemd.journal import JournalHandler
+# Pairing is necessary for mac address spoofing
+# bluetoothctl scan on pair
+# logs at journalctl -b -u keyless.service -f
 
-# from open_close import unlock_door, lock_door, reset_relays
-
-# import bt_manager
-# import dbus
-# import gobject
-# from dbus.mainloop.glib import DBusGMainLoop
-# import re
-# import syslog
-
-# adapter = bt_manager.BTAdapter()
 
 target_address = "5C:17:CF:D4:EE:6B"  # shuhrats phone
 # target_address = "88:66:5A:C7:3F:DA"  # shuhrats ipad
-deviceInRange = True
 lastState = "away"  # host down
-currentState = "connected"
-
-# pins
-lock = 3  # pin 9 on harness, IN2, GPIO3
-unlock_init = 17  # pin 1 on harness, IN4, GPIO17
-unlock = 4  # pin 2 on harness, IN3, GPIO4
-light = 2  # pin 4 on harness, IN1, GPIO2
 sleepTime = 2
 
-# unlock seq - pin 1 ground, pin 2 ground
-# light - pin 4 ground
-# lock - pin 9 ground
 
-# Pairing is necessary for mac address spoofing
-# sudo service bluetooth restart
-# sudo hciconfig
-# sudo hciconfig hci0 piscan
-# sudo bluez-simple-agent
+
+# get an instance of the logger object this module will use
+logger = logging.getLogger(__name__)
+
+# instantiate the JournaldLogHandler to hook into systemd
+journald_handler = JournalHandler()
+
+# set a formatter to include the level name
+journald_handler.setFormatter(logging.Formatter(
+    '[%(levelname)s] %(message)s'
+))
+
+# add the journald handler to the current logger
+logger.addHandler(journald_handler)
+
+# optionally set the logging level
+logger.setLevel(logging.DEBUG)
 
 while True:
     try:
         btsocket = bluetooth.BluetoothSocket(bluetooth.L2CAP)
         btsocket.connect((target_address, 3))
         if btsocket.send("init") and lastState == "away":
-            print("device found %s" % (target_address))
-            print("Unlocking car...")
-            #   unlock_door(lock,unlock_init, unlock, light)
+            logger.info("device found %s" % (target_address))
+            logger.info("Unlocking car...")
             UnlockCar()
-            print("unlock")
+            logger.info("unlock")
             lastState = "connected"
             btsocket.close()
         time.sleep(sleepTime)
+
+    # terrible way to check error, but e.errno didn't work
     except bluetooth.btcommon.BluetoothError as e:
-        # terrible way to check error, but e.errno didn't work
         if lastState == "connected" and "112" in str(e):
-            print("Locking...", str(e))
-            #   lock_door(lock, unlock_init, unlock, light)
+            logger.info("Locking car...")
             LockCar()
-            print("lock")
+            logger.info("lock")
             lastState = "away"
         time.sleep(sleepTime)
+
+    # killing program
     except KeyboardInterrupt:
         btsocket.close()
-        # reset_relays(lock, unlock_init, unlock, light)
+        logger.info("program terminated")
         time.sleep(sleepTime)
         sys.exit(1)
+
+    # all other exceptions
     except:
-        # sys.exc_info()[0]
-        # lock_door(lock, unlock_init, unlock, light)
         LockCar()
-        print("lock")
+        logger.info("lock")
         btsocket.close()
